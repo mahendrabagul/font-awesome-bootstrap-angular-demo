4b15c51135f0df4a5e5a0ffdbf2266ffa4f935c2
Mahendra Bagul <bagulm123@gmail.com>
2018-01-14 23:40:51 +0530

    Added header and profile components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    ProfileInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
