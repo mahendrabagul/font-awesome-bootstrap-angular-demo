ba9e99fdeb0a47d7b73465ffb222680d8f9efc19
Mahendra Hiraman <--edit>
2018-01-15 18:52:43 +0530

    changed code by Ganesh

--- src/app/profile-info/profile-info.component.ts
+++ src/app/profile-info/profile-info.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-info.component.css']
 })
 export class ProfileInfoComponent implements OnInit {
-  jobTitle: string = "Specialist Programmer @ Infosys";
-  profileImageUrl:string="https://avatars2.githubusercontent.com/u/6149190?s=460&v=4";
+  currentJobTitle: string = "Specialist Programmer @ Infosys";
+  currentJobDetails: string = "Working on Spring Boot, Micro-services, JHipster, Angular 5, Typescript, Spring-Social Integrations, Oauth2, Elastic Search, Cassandra, CouchDB, Jersey, CI/CD Pipeline, Bamboo, Bitbucket, SonarQube, Jira and many more tools for Agile Development.";
+  profileImageUrl: string = "http://localhost:3000/images/profile.jpg";
   constructor() { }
 
   ngOnInit() {
