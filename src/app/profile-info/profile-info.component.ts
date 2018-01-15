aebc5b20f7ad96db0b29db8bbf8e4d7b234482ee
Mahendra Hiraman <--edit>
2018-01-15 17:56:32 +0530

    Added Font awesome css

--- src/app/profile-info/profile-info.component.ts
+++ src/app/profile-info/profile-info.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-info.component.css']
 })
 export class ProfileInfoComponent implements OnInit {
-
+  jobTitle: string = "Specialist Programmer @ Infosys";
+  profileImageUrl:string="https://avatars2.githubusercontent.com/u/6149190?s=460&v=4";
   constructor() { }
 
   ngOnInit() {
